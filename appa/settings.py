2aba5b6ddc57e9342ece75b7d3f6cf8e8d941fcd
lbte <lbustamante@unal.edu.co>
2023-11-11 16:34:44 -0500

    Fix base user and replace secrete key by environment variable

--- appa/settings.py
+++ appa/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n9)io)9(m2z95e0p(@@qvw45%oz=oi(kle0i2&&7mf+352_9tz'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- appa_admin/models/manager.py
+++ appa_admin/models/manager.py
@@ -26,7 +26,6 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
-        extra_fields.setdefault("validado", True)
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError(_("Superuser must have is_staff=True."))
