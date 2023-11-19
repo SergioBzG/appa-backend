ed6d83bbf081a3a492b1af6dc089a631d53a516c
Laura Bustamante <lbustamante@unal.edu.co>
2023-11-19 14:31:45 -0500

    Add endpoints for update and delete profile

--- appa_admin/urls.py
+++ appa_admin/urls.py
@@ -3,4 +3,6 @@ from .views.user_views import *
 
 urlpatterns = [
   path("user/services/<int:user_id>/", get_user_services, name="get_user_services"),
+  path("user/update/<int:user_id>/", update_user_profile, name="update_user_profile"),
+  path("user/delete/<int:user_id>/", delete_user_profile, name="delete_user_profile"),
 ]
--- appa_admin/views/user_views.py
+++ appa_admin/views/user_views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from authentication.permissions.role_permissions import IsCitizen
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
@@ -7,6 +8,7 @@ from authentication.permissions.is_token_valid import IsTokenValid
 from services.models import Service
 from services.serializers.service_serializer import ServiceSerializer
 from ..models.user import User
+from ..serializers.user_serializer import UserSerializer
 
 
 @api_view(["GET"])
@@ -54,4 +56,72 @@ def get_user_services(request, user_id: int) -> JsonResponse:
         return JsonResponse(
             data={"message": "User does not exist"},
             status=status.HTTP_404_NOT_FOUND
+        )
+    
+@api_view(["PATCH"])
+@permission_classes([IsAuthenticated, IsTokenValid, ~IsAdminUser])
+def update_user_profile(request, user_id: int) -> JsonResponse:
+    try:
+        user: User = User.objects.get(pk=user_id)
+        if user != request.user:
+            return JsonResponse(
+                data={"message": "Incorrect user id"},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        
+        if request.data["name"]:
+            user.name = request.data["name"]
+        if request.data["email"]:
+            user.email = request.data["email"]
+        if request.data["phone"]:
+            user.phone = request.data["phone"]
+        if request.data["password"]:
+            user.password = request.data["password"]
+
+        user.save(update_fields=["name", "email", "phone", "password"])
+        user_serializer : UserSerializer = UserSerializer(user).data
+        data: dict = {"message": "User updated successfully", "user": user_serializer.data}
+        return JsonResponse(data=data, status=status.HTTP_200_OK)
+        
+    except User.DoesNotExist:
+        return JsonResponse(
+            data={"message": "User does not exist"},
+            status=status.HTTP_404_NOT_FOUND
+        )
+    except Exception as e:
+        return JsonResponse(
+            data={"error message": str(e)},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+    
+
+@api_view(["DELETE"])
+@permission_classes([IsAuthenticated, IsTokenValid, IsCitizen | IsAdminUser])
+def delete_user_profile(request, user_id: int) -> JsonResponse:
+    try:
+        user: User = User.objects.get(pk=user_id)
+        if user != request.user:
+            if request.user.is_staff:
+                pass
+            else:
+                return JsonResponse(
+                    data={"message": "Incorrect user id"},
+                    status=status.HTTP_403_FORBIDDEN
+                )
+            
+        user.delete()
+        return JsonResponse(
+            data={"message": "User deleted"},
+            status=status.HTTP_204_NO_CONTENT
+        )
+    
+    except User.DoesNotExist:
+        return JsonResponse(
+            data={"message": "User does not exist"},
+            status=status.HTTP_404_NOT_FOUND
+        )
+    except Exception as e:
+        return JsonResponse(
+            data={"error message": str(e)},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR
         )
