f21c028224c07da8b47e1dbb7c7ba7388cf54830
Sergio <sebermudezg@unal.edu.co>
2023-11-21 01:46:10 -0500

    Custom token with user id, role and name

--- appa_admin/models/manager.py
+++ appa_admin/models/manager.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import gettext_lazy as _
 
+from authentication.models import Role
+
 
 class UserManager(BaseUserManager):
 
@@ -26,6 +28,14 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
+
+        # Set role to AVATAR by default
+        role: Role = Role.objects.filter(name="AVATAR").first()
+        if role:
+            extra_fields.setdefault("role", role)
+        else:
+            role = Role.objects.create(name="AVATAR", description="Manager of the system")
+            extra_fields.setdefault("role", role)
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError(_("Superuser must have is_staff=True."))
---
+++ authentication/serializers/token_serializer.py
@@ -0,0 +1,13 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['user_name'] = user.name
+        token['user_role'] = user.role.name
+
+        return token
