b68e1e7bc02db7d7e062270aa910ae34add97255
lbte <lbustamante@unal.edu.co>
2023-11-26 21:05:40 -0500

    Add get bison's last service function and url

--- appa_admin/urls.py
+++ appa_admin/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     re_path(r"^user/services/(?P<user_id>\d+)(?:/(?P<type>package|carriage))?/$",
             get_user_services,
             name="get_user_services"),
+    path("user/last-service/<int:user_id>/",
+         get_bison_last_service,
+         name="get_bison_last_service"
+         ),
 ]
--- appa_admin/views/user_views.py
+++ appa_admin/views/user_views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from authentication.permissions.is_token_valid import IsTokenValid
+from authentication.permissions.role_permissions import IsBison
 from services.models import Service
 from services.serializers.service_serializer import ServiceSerializer
 from ..models.user import User
@@ -43,6 +44,41 @@ def get_user_services(request, user_id: int, type: str = "") -> JsonResponse:
             status=status.HTTP_200_OK
         )
 
+    except User.DoesNotExist:
+        return JsonResponse(
+            data={"message": "User does not exist"},
+            status=status.HTTP_404_NOT_FOUND
+        )
+    
+@api_view(["GET"])
+@permission_classes([IsAuthenticated, IsTokenValid, IsBison])
+def get_bison_last_service(request, user_id: int) -> JsonResponse:
+    """
+
+    :param request:
+    :param user_id:
+    :param type:
+    :return:
+    """
+    try:
+        user: User = User.objects.get(pk=user_id)
+        if user != request.user:
+            return JsonResponse(
+                data={"message": "Incorrect user id"},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        
+        bison_services: list[Service] = user.bison_orders.all().order_by("created")
+        bison_last_service = bison_services[-1]
+
+        serializer: ServiceSerializer = ServiceSerializer(bison_last_service, many=True)
+
+        return JsonResponse(
+            data=serializer.data,
+            safe=False,
+            status=status.HTTP_200_OK
+        )
+        
     except User.DoesNotExist:
         return JsonResponse(
             data={"message": "User does not exist"},
